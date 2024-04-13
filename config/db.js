9011c5179bac54f14a7ed6c3ff03ed9188fbcc1a
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-04-13 02:19:35 -0300

    database cofig

--- config/db.js
+++ config/db.js
@@ -1,10 +1,15 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("carbon", null, null, {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-  timezone: "-03:00",
-});
+const db = new Sequelize(
+  "ujhgttgs",
+  "ujhgttgs",
+  "uiM3it79Fa3gfR4P-2x-zAS_ts30L2hN",
+  {
+    host: "isilo.db.elephantsql.com",
+    dialect: "postgres",
+    logging: false,
+    timezone: "-03:00",
+  }
+);
 
 module.exports = db;
